84af5d0588d9f93dd8da22279bec45273a798c97
Adam Archer <me@adamarcher.com>
2012-04-11 01:04:33 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redpuma::Application.config.secret_token = '18125273c4a596e6cf13dbce60b7ec15ac62164a5adc07b2e0cbef27cba6cac204ead14946d77b7b270ec435e6c92f8495a6a4f1444586afde789c698d5ad016'
