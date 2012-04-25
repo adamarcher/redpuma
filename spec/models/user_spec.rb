a919269b1522aa2bf3427626aab97d8dfc1f1920
Adam Archer <me@adamarcher.com>
2012-04-25 13:39:47 -0400

    All changes up to 6.2.2 'length validation'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,3 +21,15 @@ describe User do
   end
 
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
