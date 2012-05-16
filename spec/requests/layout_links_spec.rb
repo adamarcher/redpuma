e4041534b7f699f1959ad0845a93254336eca841
Adam Archer <me@adamarcher.com>
2012-05-15 23:18:34 -0400

    Fixing rspec tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -58,12 +58,6 @@ describe "LayoutLinks" do
 					 :content => "Sign out")
     end
 
-    it "should have a profile link" do
-      visit root_path
-      response.should have_selector("a", :href => user_path(@user),
-					 :content => "Profile")
-    end
-
   end  # describe "when signed in" do
 
 end  # describe "LayoutLinks" do
